8231cb174993f85fa3b52c1d96eb92619f6b4e38
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-04 15:59:03 -0500

    step:1 mongodb setup

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const { Schema } = mongoose;
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+});
+
+export const User = mongoose.model("User", UserSchema);
