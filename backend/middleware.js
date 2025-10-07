3a046940cff867b34f220a0511c214570094a289
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-06 22:08:55 -0500

    step 8

--- backend/middleware.js
+++ backend/middleware.js
@@ -11,7 +11,7 @@ export function authMiddleware(req, res, next) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       console.log(decoded.userId);
-      req.userId = decoded.userId;
+      req.body.userId = decoded.userId;
       next();
     } catch (err) {
       return res.status(403).json({ msg: "not authenticated" });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ import express from "express";
 import { z } from "zod";
 import { User } from "../db.js";
 import jwt from "jsonwebtoken";
+import { authMiddleware } from "../middleware.js";
 
 const userSchema = z.object({
   username: z.string().min(3).max(30),
@@ -14,6 +15,13 @@ const signinSchema = z.object({
   username: z.string().min(3).max(30),
   password: z.string().min(6),
 });
+
+// const updateSchema = z.object({
+//   firstName: z.string().min(3).max(30).optional(),
+//   lastName: z.string().min(3).max(30).optional(),
+//   password: z.string().min(6).optional(),
+//   userId: z.string().min(1),
+// });
 
 const userRouter = express.Router();
 
@@ -82,6 +90,44 @@ userRouter.post("/signin", async (req, res) => {
 
   const token = jwt.sign({ userId }, process.env.JWT_SECRET);
   return res.status(200).json({ token });
+});
+
+userRouter.put("/", authMiddleware, async (req, res) => {
+  const { firstName, lastName, password, userId } = req.body;
+  console.log("userId", userId);
+
+  const user = await User.findOne({ _id: userId });
+
+  if (!user) {
+    return res.status(411).json({ message: "User not found" });
+  }
+
+  if (password && password.length < 6) {
+    return res
+      .status(411)
+      .json({ message: "Password must be at least 6 characters long" });
+  }
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { firstName, lastName, password }
+  );
+
+  return res.status(200).json({ message: "Updated successfully" });
+});
+
+userRouter.get("/bulk", authMiddleware, async (req, res) => {
+  const users = req.query.filter;
+  console.log("params", users);
+  if (!users) {
+    return res.status(411).json({ message: "Please provide user ids" });
+  }
+  const userList = await User.find({ username: { $in: users } });
+
+  if (!userList) {
+    return res.status(411).json({ message: "Users not found" });
+  }
+  return res.status(200).json({ users: userList });
 });
 
 export default userRouter;
