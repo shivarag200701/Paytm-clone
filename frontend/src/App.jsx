bb647766a35e08e07925ebb371a8e2754f57398b
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-09 12:32:55 -0500

    feat: changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./Components/Signup";
+import SignIn from "./Components/SignIn";
+import Dashboard from "./Components/Dashboard";
+import Send from "./Components/Send";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="flex justify-center items-center shadow-sm min-h-screen">
+      <div className="bg-white w-full max-w-sm rounded-md">
+        <div className="text-4xl font-bold text-center">Sign Up</div>
+        <p className="text-center mt-2 text-[#99a1af]">
+          Enter your information to create and
+        </p>
+        <p className="text-center text-[#99a1af]">account</p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
